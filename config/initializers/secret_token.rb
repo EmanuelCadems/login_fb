bd17b56cb66f4513040218ff093a46d4f54df9b7
EmanuelCadems <emanuel_cadems@hotmail.com>
2013-06-19 18:58:06 -0300

    [Initial Commit]
      - Adding Gems: figaro, devise, omniauth-facebook.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LoginFb::Application.config.secret_key_base = '2852a3d8bc65eaf0bb165697e6d0a8ad4aa4f9020edc64f140ea3096407a44fbfd3b2c8d0897bee1872e351c1b69ef96c6c06fedae281f5c39e930537aea96cb'
